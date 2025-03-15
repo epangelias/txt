f63cc99dcece64427192ba7c72eecf74ea3078ac
Epangelias <epangelias@gmail.com>
2025-03-15 13:43:41 -0700

    first commit

---
+++ client.ts
@@ -0,0 +1,45 @@
+
+
+export default function () {
+  const textarea = document.querySelector("textarea") as HTMLTextAreaElement;
+  const title = document.querySelector("title") as HTMLTitleElement;
+
+  let lastContent = textarea.value;
+  let saved = true;
+  let saving = false;
+  let lastSaved = Date.now();
+  let lastChanged = 0;
+
+  textarea.oninput = () => lastChanged = Date.now();
+
+  function update() {
+    saved = lastContent == textarea.value;
+    const firstLine = textarea.value.split("\n")[0];
+    title.textContent = (saved ? "" : "* ") + (firstLine || "TXT");
+
+    if (Date.now() - lastSaved > 5000
+      && Date.now() - lastChanged > 1000
+      && !saving
+      && !saved) save(textarea.value);
+
+    requestAnimationFrame(update);
+  }
+  requestAnimationFrame(update);
+
+  async function save(content: string) {
+    try {
+      saving = true;
+      const res = await fetch("", { method: "POST", body: content });
+      if (!res.ok) throw res.statusText;
+      lastSaved = Date.now();
+      lastContent = content;
+    } catch (e) {
+      console.error(e);
+    }
+    finally {
+      lastSaved = Date.now();
+      saving = false;
+      update();
+    }
+  }
+}
---
+++ main.tsx
@@ -0,0 +1,53 @@
+/// <reference lib="deno.unstable" />
+
+import { render } from "preact-render-to-string";
+
+const db = await Deno.openKv();
+async function page(code: string) {
+
+  const res = await db.get<string>(["content", code]);
+  const content = res.value || "TXT";
+
+  const title = content.split("\n")[0] ?? "TXT";
+
+  const css = await Deno.readTextFile("./style.css");
+  const fun = (await import('./client.ts')).default.toString();
+  const js = `(${fun})()`;
+
+  return <html>
+    <head>
+      <title>{title}</title>
+      <meta name="color-scheme" content="light dark" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name='mobile-web-app-capable' content='yes' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta name='theme-color' content="#000000" />
+    </head>
+    <body>
+      <textarea placeholder="..." autocomplete="on">{content}</textarea>
+      <style dangerouslySetInnerHTML={{ __html: css }}></style>
+      <script dangerouslySetInnerHTML={{ __html: js }}></script>
+    </body>
+  </html>
+}
+
+async function handler(req: Request) {
+  const url = new URL(req.url);
+  const code = url.pathname.slice(1);
+  if (req.method == "GET") {
+    const html = "<!DOCTYPE html>\n" + render(await page(code));
+    return new Response(html, { headers: { "Content-Type": "text/html" } });
+  } else {
+    try {
+      const text = await req.text();
+      const res = await db.set(["content", code], text);
+      if (!res.ok) throw new Error("Failed to save");
+      return new Response();
+    } catch (e) {
+      console.error(e);
+      return Response.error();
+    }
+  }
+}
+
+Deno.serve(handler);
